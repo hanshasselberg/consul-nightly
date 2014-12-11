5056f476f7bfef8826521c338879fd6dbcb5a956
Hans Hasselberg <me@hans.io>
2014-12-11 09:55:07 +0100

    use golang 1.4

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ name=$2
 sudo apt-get update &> /dev/null
 sudo apt-get --assume-yes --force-yes install git build-essential python-pip libsqlite3-dev &> /dev/null
 sudo pip install awscli
-curl -L http://golang.org/dl/go1.3.3.$name.tar.gz -O
+curl -L http://golang.org/dl/go1.4.$name.tar.gz -O
 tar xvzf go1.3.3.$name.tar.gz &> /dev/null
 echo "Host github.com" >> ~/.ssh/config
 echo " StrictHostKeyChecking no" >> ~/.ssh/config
--- build_local.sh
+++ build_local.sh
@@ -7,7 +7,7 @@ revision=$1
 name="darwin-amd64-osx10.8"
  
 if [[ ! -d go ]]; then
-  curl -s -L http://golang.org/dl/go1.3.3.${name}.tar.gz -O
+  curl -s -L http://golang.org/dl/go1.4.${name}.tar.gz -O
   tar xvzf go1.3.3.${name}.tar.gz &> /dev/null
 fi
  
@@ -15,13 +15,18 @@ export GOROOT=`pwd`/go
 export GOPATH=`pwd`/gopath
 export PATH="`pwd`/go/bin":$PATH
  
-rm -rf $GOPATH
-mkdir -p $GOPATH/src/github.com/hashicorp/
-git clone git@github.com:hashicorp/consul.git $GOPATH/src/github.com/hashicorp/consul
-cd $GOPATH/src/github.com/hashicorp/consul
+if [[ -d $GOPATH ]]; then
+  cd $GOPATH/src/github.com/hashicorp/consul
+  git pull
+else
+  mkdir -p $GOPATH/src/github.com/hashicorp/
+  git clone git@github.com:hashicorp/consul.git $GOPATH/src/github.com/hashicorp/consul
+  cd $GOPATH/src/github.com/hashicorp/consul
+fi
 
 git reset --hard $revision
  
+go get -u ./...
 make
 echo "`pwd`/bin/consul"
 AWS_DEFAULT_REGION=eu-west-1 aws s3 cp --acl public-read "`pwd`/bin/consul" s3://consul-nightly/$revision/darwin-amd64 --profile hans.io
